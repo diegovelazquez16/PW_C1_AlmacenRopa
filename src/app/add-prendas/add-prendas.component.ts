039fa80f03e5907038e09bc8370f5770c9300a8b
Diego Velazquez <govelazmen@gmail.com>
2024-10-03 03:00:17 -0600

    Add: CRUD de empleados

---
+++ src/app/add-prendas/add-prendas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-prendas',
+  templateUrl: './add-prendas.component.html',
+  styleUrl: './add-prendas.component.css'
+})
+export class AddPrendasComponent {
+
+}
---
+++ src/app/add-proveedores/add-proveedores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-proveedores',
+  templateUrl: './add-proveedores.component.html',
+  styleUrl: './add-proveedores.component.css'
+})
+export class AddProveedoresComponent {
+
+}
